5e57b9f2c825c7514ee4727301428ac47ddd59f5
AzeezMuhammed <Azeezanafi22@gmail.com>
2023-09-29 20:59:34 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const menubtn=document.getElementById("menu")
+const navbar=document.getElementById("nav")
+menubtn.addEventListener("click",()=>{
+    navbar.classList.toggle('active');
+ 
+//  if(navbar.style.display=="block"){
+    //  navbar.style.display="none";
+//  }else{
+    //  navbar.style.display="block";
+//  }
+})
